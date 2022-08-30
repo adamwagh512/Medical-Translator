72461ecfc029d0c4deb5eda4cd1a29ecec8482c1
Adam Wagh <adamwagh@gmail.com>
2022-08-30 15:08:59 -0500

    basic backend build

---
+++ server/routes/api/userRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  updateUser,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/')
+.get(getUsers)
+.post(createUser);
+
+// /api/users/:userId
+router.route('/:_id')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
+
+module.exports = router;
